2a628e70be866fe1fabb21094791ae30cf6eb19e
Yamkela ZUNGULA <yzungula@c4r11s7.wethinkcode.co.za>
2019-06-03 17:21:07 +0200

    Updated phonebook

--- ex01/phonebook.class.cpp
+++ ex01/phonebook.class.cpp
@@ -16,3 +16,73 @@ std::string Phonebook::getFirstname() const
 {
     return (firstname);
 }
+
+void Phonebook::setLastname(std::string lname)
+{
+    lastname = lname;
+}
+
+std::string Phonebook::getLastname() const
+{
+    return (lastname);
+}
+
+void Phonebook::setNickname(std::string name)
+{
+    nickname = name;
+}
+
+std::string Phonebook::getNickname() const
+{
+    return (nickname);
+}
+
+void Phonebook::setLogin(std::string loginString)
+{
+    login = loginString;
+}
+
+std::string Phonebook::getLogin() const
+{
+    return (login);
+}
+
+void Phonebook::setPostalAddress(std::string address)
+{
+    postalAddress = address;
+}
+
+std::string Phonebook::getPostalAddress() const
+{
+    return (postalAddress);
+}
+
+std::string Phonebook::formatAddress()
+{
+    return (postalAddress);
+}
+
+void Phonebook::setEmail(std::string mail)
+{
+    email = mail;
+}
+
+std::string Phonebook::getEmail() const
+{
+    return (email);
+}
+
+void Phonebook::setPhoneNumber(std::string number)
+{
+    if (number.length() == 10)
+        phoneNumber = number;
+    else
+    {
+        std::cout << "Phone number must be 10 digits\n";
+    }
+}
+
+std::string Phonebook::getPhoneNumber() const
+{
+    return (phoneNumber);
+}
